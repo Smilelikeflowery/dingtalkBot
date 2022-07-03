5334cb41a98800f6cfb5ea4021caeec74c945438
Smilelikeflowery <99317081+Smilelikeflowery@users.noreply.github.com>
2022-07-04 00:12:04 +0800

    update 0704

--- main.js
+++ main.js
@@ -3,8 +3,11 @@
 const defaultOptions = {
   baseUrl: 'https://oapi.dingtalk.com/robot/send',
   accessToken:
-    'd49fd2eb77d0a96520a3ec6261b514f02cee6e61b06d9a49723338696683777e',
-  secret: 'SECeb8f227ee9c919d68d171cf2a3cb1949b26a971616dcbd4e687ca56317e96066',
+    'e139c0d0cea4f0f18b5a36cc937de7c4031d1aa85d2ed2ae6ddc72f174394c96',
+  secret: 'SEC8ed9bc6f872824e7b1befeebdef6ad2fa33c6270fba2b371f7b80279319aa3d6',
+  // accessToken:
+  //   'd49fd2eb77d0a96520a3ec6261b514f02cee6e61b06d9a49723338696683777e',
+  // secret: 'SECeb8f227ee9c919d68d171cf2a3cb1949b26a971616dcbd4e687ca56317e96066',
 };
 const robot = new ChatBot(defaultOptions);
 
