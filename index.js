8505a62367e57fdb1cdb28d1d885c4dadce899ba
Smilelikeflowery <99317081+Smilelikeflowery@users.noreply.github.com>
2022-07-05 00:39:48 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,74 +1,33 @@
 ﻿const { sendText, sendImage, sendMarkdown } = require('./main');
 const githubReadme = require('github-readme');
-// const axios = require('axios').default;
-// const baseUrl = 'https://www.mxnzp.com/api/weather/current/';
-// const imgUrl = "https://www.dmoe.cc/random.php?return=json";
-// const djtUrl = "https://api.btstu.cn/yan/api.php?charset=utf-8&encode=json";
 
-// function getWeather(city, appId, appSecret, callback) {
-//   const url = `${baseUrl}${encodeURI(
-//     city
-//   )}?app_id=${appId}&app_secret=${appSecret}`;
-//   axios.get(url).then((res) => {
-//     callback(res.data.data);
-//   });
-// }
-// getWeather(
-//   '天津市',
-//   'oqpukkqmpismtwrq',
-//   'UEsyWXovZlRjSTl0dUJyRjFvK2xMQT09',
-//   sendWeather
-// );
+function sendMsg() {
+  return new Promise((resolve, reject) => {
+    githubReadme('Smilelikeflowery', 'dingtalkBot', (err, data) => {
+      resolve(data);
+    });
+  });
+}
 
-// function sendWeather(info) {
-//   let wInfo = `
-//      地区: ${info.address}
-//      温度: ${info.temp}
-//      天气状况: ${info.weather}
-//      风向: ${info.windDirection}
-//      风力: ${info.windPower}
-//      湿度: ${info.humidity}
-//      报告时间: ${info.reportTime}
-//   `;
-//   sendMarkdown('今日天气状况', wInfo);
-// }
-
-// function getImage(callback){
-//   axios.get(imgUrl).then(res => {
-//     // console.log(res.data.imgurl);
-//     if (res) {
-//     callback("每日一图！",`![](${res.data.imgurl})`)
-//     } else {
-//       callback("每日一图","获取图片失败！")
-//     }
-//   })
-// }
-
-// getImage(sendMarkdown)
-
-
-// function getDJT(callback) {
-//   axios.get(djtUrl).then(res => {
-//     if (res.data.text) {
-//     callback(res.data.text)
-//     } else {
-//       callback("获取毒鸡汤失败！")
-//       }
-//   })
-//  }
-
-//  getDJT(sendText)
-
-githubReadme('Smilelikeflowery', 'dingtalkBot', (err, data) => {
+async function call() {
+  const data = await sendMsg();
+  const arr = [];
   if (data) {
     for (let i in data.split('$')) {
-      if (i <= 2) {
-        let img = data.split('$')[i];
-        sendMarkdown('每日早报', img);
-      } else {
-        let content = data.split('$')[i];
-        sendText(content);
-      }
+      arr.push(data.split('$')[i]);
     }
   }
-});
+  sendMarkdown('每日早报', arr[1])
+    .then(() => {
+      setTimeout(() => {
+        sendMarkdown('每日早报', arr[2]);
+      }, 1500);
+    })
+    .then(() => {
+      setTimeout(() => {
+        sendText(arr[3]);
+      }, 4000);
+    });
+}
+
+call();
